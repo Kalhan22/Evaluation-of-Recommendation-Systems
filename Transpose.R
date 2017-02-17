6ac2afde960c8ef957ada6cadb90ecc975723a53
Kalhan Dhar <dhar.kalhan@gmail.com>
2017-02-16 19:03:46 -0500

    Update Transpose.R

--- Transpose.R
+++ Transpose.R
@@ -1,5 +1,5 @@
 ##Trasnpose the Data to get the Item to Item Colloborative Filtering
-all_movie_data <- read.csv("/Users/Kalhan/Desktop/Waterloo Data/CS 846/CS 846 Project/FACT_RATING_PUSHED.csv")
+all_movie_data <- read.csv("<Location>/FACT_RATING_PUSHED.csv")
 nrow(all_movie_data)
 head(all_movie_data)
 
@@ -7,7 +7,7 @@ install.packages('reshape')
 library(reshape)
 t(all_movie_data)
 getwd()
-setwd("/Users/Kalhan/Desktop/Waterloo Data/CS 846/CS 846 Project/")
+setwd("<Location>")
 transposed_data <- reshape(all_movie_data, direction = "wide", idvar="movie_id", timevar="user_id")
 
 write.table(transposed_data,file = "FACT_RATING_transposed.csv",sep = ",",na = "")
@@ -15,10 +15,5 @@ write.table(transposed_data,file = "FACT_RATING_transposed.csv",sep = ",",na = "
 ##install.packages('RMySQL')
 ##library('RMySQL')
 ##con <- dbConnect(MySQL(),
-##                 user="root", password="mickey1992",
-##                 dbname="dwh", host="localhost")
-##Test Random Query
-##Useful Link https://www.r-bloggers.com/mysql-and-r/
-##rs <- dbSendQuery(con, "select * from DWH.FACT_RATINGS LIMIT 10;")
-##data <- fetch(rs)
-##Testing ends
+##                 user="****", password="*****",
+##                 dbname="***", host="****")
